4a701ca0f1baecc37737fa41896e5c71e6a43a46
Murilo Matias <m.matias@ufms.br>
2023-06-14 23:32:21 -0400

    feat: implementado middleware para logs

--- producer/app/main.py
+++ producer/app/main.py
@@ -16,6 +16,35 @@ class HorarioPonto(BaseModel):
 app = FastAPI()
 producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
 
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    
+    headers = dict(request.headers)
+    user_agent = headers.get('user-agent')
+    
+    body = await request.body()
+    
+    response = await call_next(request)
+    
+    process_time = time.time() - start_time
+    
+    log_data = {
+        'method': request.method,
+        'path': request.url.path,
+        'headers': headers,
+        'user-agent': user_agent,
+        'request_body': body.decode(),
+        'status_code': response.status_code,
+        'response_body': await response.text(),
+        'process_time': process_time,
+    }
+    
+    # log the data using your preferred logging library
+    logger.info(log_data)
+    
+    return response
+
 @app.post("/send")
 async def send_notification(message: Message):
     try:
