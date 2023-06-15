4122d6824fc185497cb5b6edceb263cad8007f54
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2023-06-15 00:18:29 -0300

    feat: adicionando endpoint enviar_horario_ponto

--- producer/app/main.py
+++ producer/app/main.py
@@ -6,6 +6,12 @@ from kafka import KafkaProducer
 class Message(BaseModel):
     text: str
 
+class HorarioPonto(BaseModel):
+    id: str
+    id_funcionario: str
+    data_hora: str
+    tipo: str
+    localizacao: str
 
 app = FastAPI()
 producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
@@ -17,3 +23,11 @@ async def send_notification(message: Message):
         return {"message": "Notification sent in the background"}
     except e: 
         return {"message": "Notification failed to send", "error": e}
+
+@app.post('/enviar_horario_ponto')
+async def enviar_horario_ponto(horario_ponto: HorarioPonto):
+    try:
+        producer.send('horario_ponto', value=horario_ponto.json().encode())
+        return {"message": "Horario de ponto enviado com sucesso"}
+    except e: 
+        return {"message": "Falha ao enviar horario de ponto", "error": e}
