8cdb1a22f17706d162044ddec18382fc90d3c4e6
Murilo Matias <m.matias@ufms.br>
2023-06-17 13:32:52 -0400

    fix: adicionando atributo em falta

--- producer/app/main.py
+++ producer/app/main.py
@@ -13,9 +13,10 @@ class HorarioPonto(BaseModel):
     data_hora: str
     tipo: str
     localizacao: str
-    dep_func: str
+    dep_funcionario: str
     setor_funcionario: str
     unidade_funcionario: str
+    empresa_funcionario: str
 
 
 class ComprovanteDeMatricula(BaseModel):
